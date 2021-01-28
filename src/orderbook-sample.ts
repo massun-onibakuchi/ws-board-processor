d5d7d5ff3e73f9a1b326d440051199ec73a65ed9
bakuchi- <helveticaf0015se@gmail.com>
2021-01-28 12:23:58 +0900

    Fixed

--- src/orderbook-sample.ts
+++ src/orderbook-sample.ts
@@ -1,5 +1,5 @@
 import FTXWs from 'ftx-api-ws';
-import { realtime } from './update-orderbook';
+import { realtime, ReadOrderBook } from './update-orderbook';
 // including private channels:
 // const ftx = new FTXWs({
 //   key: 'x',
@@ -9,13 +9,20 @@ import { realtime } from './update-orderbook';
 
 // only public channels:
 const ftx = new FTXWs() as any;
+const boardReader = new ReadOrderBook();
+let board = { asks: new Map(), bids: new Map() };
+class BoardManagment extends FTXWs {
+    board;
+}
+const manager = new BoardManagment()
 
 const go = async () => {
     await ftx.connect();
 
     ftx.subscribe('orderbook', 'BTC-PERP');
     // ftx.on('BTC-PERP::orderbook', console.log);
-    ftx.on('BTC-PERP::orderbook',responce => realtime(responce,board));
+    // ftx.on('BTC-PERP::orderbook', responce => realtime(responce, board));
+    ftx.on('BTC-PERP::orderbook', boardReader.realtime);
 
     // if you passed api credentials:
     ftx.subscribe('fills');
--- src/update-orderbook.ts
+++ src/update-orderbook.ts
@@ -3,9 +3,9 @@ export interface ResponeBook { asks: number[][], bids: number[][], action: 'part
 
 let board = { asks: new Map(), bids: new Map() };
 
-export const realtime = (responce: ResponeBook, /* board:BoardInterface */) => {
+export const realtime = (responce: ResponeBook, board: BoardInterface) => {
     // if (responce['channel'] == 'orderbook') {
-    let board = { asks: new Map(), bids: new Map() };
+    // let board = { asks: new Map(), bids: new Map() };
     // function processBoard(board): BoardInterface {
     if (responce['action'] == 'partial') {
         board = reformatBoard(responce);
@@ -46,4 +46,44 @@ const updateBoard = (data: ResponeBook, board: BoardInterface): BoardInterface =
         }
     }
     return board
+}
+
+export class ReadOrderBook {
+    board: BoardInterface;
+
+    public realtime = (responce: ResponeBook) => {
+        if (responce['action'] == 'partial') {
+            this.board = reformatBoard(responce);
+        }
+        if (responce['action'] == 'update') {
+            this.board = updateBoard(responce, this.board);
+        }
+        console.log('board :>> ', this.board);
+    }
+    protected reformatBoard = (data: ResponeBook): BoardInterface => {
+        const board: BoardInterface = { asks: new Map(), bids: new Map() };
+        for (const key of Object.keys(data)) {
+            if (key == 'bids' || key == 'asks') {
+                data[key].forEach(quate => {
+                    board[key].set(quate[0], quate[1]);
+                })
+            }
+        }
+        return board;
+    }
+    protected updateBoard = (data: ResponeBook, board: BoardInterface): BoardInterface => {
+        for (const key of Object.keys(data)) {
+            if (!(key in ['bids', 'asks'])) return
+            for (const [price, size] of data[key]) {
+                if (board[key].has(price)) {
+                    if (size == 0) board[key].delete(price);
+                    else board[key].set(price, size);
+                }
+                else if (size > 0) {
+                    board[key].set(price, size);
+                }
+            }
+        }
+        return board
+    }
 }
