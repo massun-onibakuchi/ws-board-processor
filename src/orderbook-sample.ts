da36c79b8c1cbff1ba7f8fc90500f557ccbfe88a
bakuchi- <helveticaf0015se@gmail.com>
2021-01-30 18:38:19 +0900

    fix boardManagment class

--- src/orderbook-sample.ts
+++ src/orderbook-sample.ts
@@ -1,6 +1,4 @@
-import EventEmitter from 'events';
-import FTXWs from 'ftx-api-ws';
-import { realtime, ReadOrderBook } from './update-orderbook';
+import { BoardInterface, ResponeBook, BoardManagment } from './update-orderbook';
 // including private channels:
 // const ftx = new FTXWs({
 //   key: 'x',
@@ -9,44 +7,15 @@ import { realtime, ReadOrderBook } from './update-orderbook';
 // })
 
 // only public channels:
-const ftx = new FTXWs() as any;
-const boardReader = new ReadOrderBook();
-let board = { asks: new Map(), bids: new Map() };
-class BoardManagment extends FTXWs {
-    [x: string]: any;
-    board: any;
-    reader = new ReadOrderBook();
-    constructor() {
-        super();
-        // this.on('BTC-PERP::orderbook', this.reader.realtime);
-    }
-}
-const manager = new BoardManagment() as any;
-// const collectData = async () => {
-//     await manager.connect();
-//     manager.subscribe('orderbook', 'BTC-PERP');
-//     // manager.on('BTC-PERP::orderbook',);
-//     // manager.on('BTC-PERP::orderbook', (res) => boardReader.realtime(res));
-//     // if you passed api credentials:
-//     manager.subscribe('fills');
-//     manager.on('fills', console.log);
-
-//     // if you want to know when the status of underlying socket changes
-//     manager.on('statusChange', console.log);
-// }
-// collectData();
+const ftx = new BoardManagment() as any;
 const go = async () => {
-    await ftx.connect();
-    ftx.subscribe('orderbook', 'BTC-PERP');
-    // ftx.on('BTC-PERP::orderbook', console.log);
-    ftx.on('BTC-PERP::orderbook', responce => realtime(responce, board));
-    // ftx.on('BTC-PERP::orderbook', (res) => boardReader.realtime(res));
-
+    await ftx.ws.connect();
+    ftx.ws.subscribe('orderbook', 'BTC-PERP');
+    ftx.ws.on('BTC-PERP::orderbook', (res: ResponeBook) => ftx.realtime(res));
     // if you passed api credentials:
-    ftx.subscribe('fills');
-    ftx.on('fills', console.log);
-
+    ftx.ws.subscribe('fills');
+    ftx.ws.on('fills', console.log);
     // if you want to know when the status of underlying socket changes
-    ftx.on('statusChange', console.log);
+    ftx.ws.on('statusChange', console.log);
 }
 go();
--- src/update-orderbook.ts
+++ src/update-orderbook.ts
@@ -1,62 +1,20 @@
+import FTXWs from 'ftx-api-ws';
+
 export interface BoardInterface { asks: Map<number, number>, bids: Map<number, number> };
 export interface ResponeBook { asks: number[][], bids: number[][], action: 'partial' | 'update', timestamp: number };
 
-let board = { asks: new Map(), bids: new Map() };
-
-export const realtime = (responce: ResponeBook,  board: BoardInterface) => {
-    // if (responce['channel'] == 'orderbook') {
-    // let board = { asks: new Map(), bids: new Map() };
-    // function processBoard(board): BoardInterface {
-    if (responce['action'] == 'partial') {
-        board = reformatBoard(responce);
+export class BoardManagment {
+    board: any;
+    ws: FTXWs;
+    constructor(config = {}) {
+        this.ws = new FTXWs(config);
     }
-    if (responce['action'] == 'update') {
-        board = updateBoard(responce, board);
-    }
-    console.log('board :>> ', board);
-    // return board;
-    // }
-    // }
-    // board =` processBoard(board);
-
-}
-const reformatBoard = (data: ResponeBook): BoardInterface => {
-    const board: BoardInterface = { asks: new Map(), bids: new Map() };
-    for (const key of Object.keys(data)) {
-        if (key == 'bids' || key == 'asks') {
-            data[key].forEach(quate => {
-                board[key].set(quate[0], quate[1]);
-            })
-        }
-    }
-    return board;
-}
-
-const updateBoard = (data: ResponeBook, board: BoardInterface): BoardInterface => {
-    for (const key of Object.keys(data)) {
-        if (!(key in ['bids', 'asks'])) return
-        for (const [price, size] of data[key]) {
-            if (board[key].has(price)) {
-                if (size == 0) board[key].delete(price);
-                else board[key].set(price, size);
-            }
-            else if (size > 0) {
-                board[key].set(price, size);
-            }
-        }
-    }
-    return board
-}
-
-export class ReadOrderBook {
-    board: BoardInterface;
-
     public realtime = (responce: ResponeBook) => {
         if (responce['action'] == 'partial') {
-            this.board = reformatBoard(responce);
+            this.board = this.reformatBoard(responce);
         }
         if (responce['action'] == 'update') {
-            this.board = updateBoard(responce, this.board);
+            this.board = this.updateBoard(responce);
         }
         console.log('board :>> ', this.board);
     }
@@ -71,9 +29,9 @@ export class ReadOrderBook {
         }
         return board;
     }
-    protected updateBoard = (data: ResponeBook, /* board: BoardInterface */): BoardInterface => {
+    protected updateBoard = (data: ResponeBook): BoardInterface => {
         for (const key of Object.keys(data)) {
-            if (!(key in ['bids', 'asks'])) return
+            if (!(key in ['bids', 'asks'])) continue;
             for (const [price, size] of data[key]) {
                 if (this.board[key].has(price)) {
                     if (size == 0) this.board[key].delete(price);
