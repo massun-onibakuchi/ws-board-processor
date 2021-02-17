611035712255b8bedb62cf3d59d0a59ac1b33fc8
bakuchi- <helveticaf0015se@gmail.com>
2021-02-17 11:05:19 +0900

    use for await of

--- src/orderbook-sample.ts
+++ src/orderbook-sample.ts
@@ -1,6 +1,9 @@
-import { formatDiagnostic } from 'typescript';
 import { BoardInterface, ResponeBook, BoardManagment } from './update-orderbook';
 import { on } from 'events'
+import { createWriteStream } from 'fs'
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
 // including private channels:
 // const ftx = new FTXWs({
 //   key: 'x',
@@ -18,18 +21,25 @@ class Analyze {
     }
 }
 // only public channels:
+const path = './orderbook.csv'
 const ftx = new BoardManagment() as any;
-
+const stream =  createWriteStream(path)
 const go = async () => {
     await ftx.ws.connect();
     ftx.ws.subscribe('orderbook', 'BTC-PERP');
+    // ftx.ws.on('BTC-PERP::orderbook', console.log);
     // ftx.ws.on('BTC-PERP::orderbook', (res: ResponeBook) => ftx.realtime(res));
-
     for await (const event of on(ftx.ws, "BTC-PERP::orderbook")) {
         ftx.realtime(event[0])
+        await sleep(100)
     }
-    // ftx.ws.subscribe('trades', 'BTC-PERP');
+
+    ftx.ws.subscribe('trades', 'BTC-PERP');
     // ftx.ws.on('BTC-PERP::trades', console.log);
+    for await (const event of on(ftx.ws, "BTC-PERP::trades")) {
+        console.log('event :>> ', event)
+        await sleep(100)
+    }
 
     // ftx.ws.subscribe('market', 'BTC-PERP');
     // ftx.ws.on('BTC-PERP::market', console.log);
