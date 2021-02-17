bbf3c842dab2e244552a0a4a0037f801587d8f50
bakuchi- <helveticaf0015se@gmail.com>
2021-02-17 15:30:06 +0900

    fix Analyse

--- src/orderbook-sample.ts
+++ src/orderbook-sample.ts
@@ -12,47 +12,72 @@ const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 // })
 
 class Analyze {
-    constructor() {
-
-    }
     boardWidth = 100
-    public calculateDepth(board: BoardInterface) {
-        let bidDepth = 0
-        let askDepth = 0
-        for (const size of board.bids.values()) {
-            bidDepth += size
+    interval = 20
+    nextUpdate = 0
+    depths = []
+    marketOrders = []
+    liquidations = [];
+    diffBoard = [];
+
+    public update() {
+        if (this.nextUpdate > Date.now()) return
+        const lasttime = this.nextUpdate;
+        this.nextUpdate += this.interval;
+        this.depths.push({ timestamp: lasttime, bid: 0, asks: 0 });
+        this.marketOrders.push({ timestamp: lasttime, buySize: 0, sellSize: 0 })
+        this.liquidations.push({ timestamp: lasttime, buy: 0, sell: 0 })
+        this.diffBoard.push({ timestamp: lasttime, asks: 0, bids: 0 })
+    }
+    public calculateDepth(board: BoardInterface,) {
+        // if (this.nextUpdate  < Date.now()) {
+        //     depth = { timestamp: 0, bid: 0, asks: 0 };
+        //     this.depths.push(depth)
+        // }
+        // else {
+        //     depth = this.depths[this.depths.length - 1];
+        //     depth.timestamp = this.nextUpdate
+        // }
+        const depth = this.depths[this.depths.length - 1];
+        for (const key of Object.keys(depth)) {
+            if (!(key in ['bids', 'asks'])) continue;
+            for (const size of board[key].values()) {
+                depth[key] += size
+            }
         }
-        for (const size of board.bids.values()) {
-            askDepth += size
-        }
+        this.depths[this.depths.length - 1] = depth
     }
     public calculateMarketOrder(orders) {
-        const timestamp = Date.now();
-        let liquidation = { timestamp: timestamp, buy: 0, sell: 0 }
-        let marketOrders = { timestamp: timestamp, buySize: 0, sellSize: 0 }
+        // let liquidation = { timestamp: 0, buy: 0, sell: 0 }
+        // let marketOrders = { timestamp: 0, buySize: 0, sellSize: 0 }
+        const liq = this.liquidations[this.liquidations.length - 1]
+        const morders = this.marketOrders[this.marketOrders.length - 1]
         for (const ord of orders) {
             if (ord.liquidation) {
                 if (ord.side == "buy")
-                    liquidation.buy += ord.size;
-                else liquidation.sell += ord.size;
+                    liq.buy += ord.size;
+                else liq.sell += ord.size;
             }
             if (ord.side == "buy") {
-                marketOrders.buySize += ord.size
+                morders.buySize += ord.size
             }
             if (ord.side == "sell") {
-                marketOrders.sellSize += ord.size
+                morders.sellSize += ord.size
             }
         }
+        this.liquidations[this.liquidations.length - 1] = liq
+        this.marketOrders[this.marketOrders.length - 1] = morders
     }
-    public calculateDiffBoard(prevBoard: BoardInterface, currentBoard?: BoardInterface, updatedBoard?: ResponeBook) {
-        let diff = { asks: 0, bids: 0 };
+    public calculateDiffBoard(prevBoard: BoardInterface, currentBoard?: BoardInterface, updateData?: ResponeBook) {
+        const diff = this.diffBoard[this.diffBoard.length - 1]
+        // let diff = { timestamp:lasttime, asks: 0, bids: 0 };
         let board: { bids: Iterable<Iterable<number>>, asks: Iterable<Iterable<number>>, [extra: string]: any };
         if (currentBoard?.asks instanceof Map) {
             board = currentBoard;
             board.asks = [...board.asks]
             board.bids = [...board.bids]
         } else {
-            board = updatedBoard;
+            board = updateData;
         }
         if (!board) return console.log("CAN NOT CALCULATE DIFF BOARD")
         for (const key of Object.keys(board)) {
@@ -66,6 +91,7 @@ class Analyze {
                 }
             });
         }
+        this.diffBoard[this.diffBoard.length - 1] = diff;
         // for (const key of Object.keys(board)) {
         //     if (!(key in ['bids', 'asks'])) continue;
         //     board[key].forEach((size: number, price: number) => {
@@ -89,8 +115,7 @@ const go = async () => {
     // ftx.ws.on('BTC-PERP::orderbook', console.log);
     // ftx.ws.on('BTC-PERP::orderbook', (res: ResponeBook) => ftx.realtime(res));
     for await (const event of on(ftx.ws, "BTC-PERP::orderbook")) {
-        // ftx.realtime(event[0])
-        console.log('event :>> ', event[0].bids, event[0].asks);
+        ftx.realtime(event[0])
     }
 
     ftx.ws.subscribe('trades', 'BTC-PERP');
